d0fdb4864a5a3233507d41020b0ebb0962305a87
Adwithr <adwithr8277@gmail.com>
2026-07-20 00:04:50 +0530

    complete add function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import sys
 import json
 import os
+from datetime import datetime
 
 file_name = "tasks.json"
 
-#Helper functions
 def load_file():
     if os.path.exists(file_name):
         with open(file_name,'r') as file:
@@ -17,9 +17,24 @@ def save_file(database):
         json.dump(database,file,indent=4)
 
 
-def add(obj):
+def add(desc):
     database = load_file()
-    database.append(obj)
+    date = datetime.now()
+    formatted_time = date.strftime("%Y-%m-%d %H:%M:%S")
+    next_id = max([task["id"] for task in database], default=0) + 1
+    task = {
+        "id": next_id,
+        "description": desc,
+        "status": "todo",
+        "createdAt": formatted_time,
+        "updatedAt": formatted_time
+    }
+    database.append(task)
     save_file(database)
 
-    
+def main():
+    command = sys.argv[1].lower()
+    if command == 'add':
+        add(sys.argv[2])
+
+main()
